c760d9cb60d4a2c08f9764817968f2ae5ab34b13
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-05-13 16:27:59 +0200

    single page application

--- index.js
+++ index.js
@@ -2,12 +2,74 @@ import jsbookmark from "./js/bookmarks.js";
 import jsbutton from "./js/buttons.js";
 
 let bookmarks = document.querySelectorAll('[data-js="bookmark"]');
-console.log(bookmarks);
 let answerbuttons = document.querySelectorAll('[data-js="answerbutton"]');
-console.log(answerbuttons);
+let footer = document.querySelectorAll('[data-js="footer"]');
 
 answerbuttons.forEach(jsbutton);
 bookmarks.forEach(jsbookmark);
+
+let reply_click = function () {
+  let x;
+  console.log(this.id);
+  if (this.id == 0) {
+    x = '[data-js="homepage"]';
+    add();
+    remove(x);
+  } else if (this.id == 1) {
+    x = '[data-js="bookmarkpage"]';
+    add();
+    remove(x);
+  } else if (this.id == 2) {
+    x = '[data-js="createpage"]';
+    add();
+    remove(x);
+  } else if (this.id == 3) {
+    x = '[data-js="profilepage"]';
+    add();
+    remove(x);
+  }
+};
+
+function add() {
+  document
+    .querySelector('[data-js="homepage"]')
+    .classList.add("wrapper--displaynone");
+  document
+    .querySelector('[data-js="bookmarkpage"]')
+    .classList.add("wrapper--displaynone");
+  document
+    .querySelector('[data-js="createpage"]')
+    .classList.add("wrapper--displaynone");
+  document
+    .querySelector('[data-js="profilepage"]')
+    .classList.add("wrapper--displaynone");
+}
+
+function remove(x) {
+  document.querySelector(x).classList.remove("wrapper--displaynone");
+}
+
+document.querySelector('[data-js="gotohome"]').onclick = reply_click;
+document.querySelector('[data-js="gotobookmark"]').onclick = reply_click;
+document.querySelector('[data-js="gotocreate"]').onclick = reply_click;
+document.querySelector('[data-js="gotoprofile"]').onclick = reply_click;
+
+/*
+footer.children[0].addEventListener("click", () => {
+  alert("test");
+});
+
+footer.children[1].addEventListener("click", () => {
+  alert("test1");
+});
+
+footer.children[2].addEventListener("click", () => {
+  alert("test2");
+});
+
+footer.children[3].addEventListener("click", () => {
+  alert("test3");
+});
 
 /* --------- Import Export function ausgelagert
 bookmarks.forEach((bookmark) => {
